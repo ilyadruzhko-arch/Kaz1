8456cf259b4c33bf204fa41a7cb4d3b44956622a
ilyadruzhko-arch <ilya.druzhko@icloud.com>
2026-05-03 18:07:27 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+TOKEN = "7448428135:AAEQ8pVtDqSnct3iV8-tJeHmhmEoDXsM" # Токен бота
+CHANNEL_ID = -10022262175 # ID канала со ставками
+ADMINS = [73015428, 6402893] # ID админов
+BET_URL = "http://t.me/CryptoBot?start=IVNj8kxr2c" # Ссылка на счет
+CRYPTOPAY_TOKEN = "250485:AAcj63sTSOB848fmfzXYb3GyeTn7igkHZ" # Тут CryptoPay токен
+WIN_IMAGE = "win.jpeg" # Название файла с картинкой выигрыша
+LOSE_IMAGE = "lose.jpeg" # Название файла с картинкой проигрыша
+LOGS_ID = -1002189721389 # ID канала с оплатами
+RULES_LINK = "https://t.me/skladmaterialov" # Ссылка на канал с правилами
+NEWS_LINK = "https://t.me/skladmaterialov" # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/skladmaterialov" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "https://t.me/cube_bet_cas" # Ссылка на переходник
+BETS_LINK = "https://t.me/skladmaterialov" # Ссылка на канал ставок
+CASINO_NAME = "skladmaterialov" # Название казино
